a8ae741f608aa62b3d17465cde0c65aedc9028d9
Rozaxe <axel.rozobrezillac@gmail.com>
2014-05-28 13:49:21 -0400

    Add click for infos

--- js/quartier.js
+++ js/quartier.js
@@ -1,4 +1,19 @@
 
+// Display Infos
+$(function() {
+    // Hide Panel
+    $('.panel-heading').first().children('i').click(function() {
+        $('.panel').first().css('display', 'none')
+    })
+
+    // Show Panel with info
+    $('.dropdown-menu').children('li').click(function() {
+        //$(this).children('a').html()
+        $('.panel').css('display', 'block')
+    })
+})
+
+// Display Map
 $(function() {
     var top = false
 
