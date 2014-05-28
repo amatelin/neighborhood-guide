87a3763f3f09570fc486acd7ca21c8ec602b7197
WidCed <cedric.wident@gmail.com>
2014-05-28 14:52:01 -0400

    Quick Fix js

--- js/quartier.js
+++ js/quartier.js
@@ -21,5 +21,6 @@ $(function() {
         $('#map').css('top', top ? '100%' : '64px')
         $('#map').find('.right').css('opacity', top ? '0' : '1')
         top = !top
+        $('.panel').first().css('display', 'none')
     })
 })
