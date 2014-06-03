5e93fdd504c1d361255e13fd0704de2099b5d9dc
Philippe Dorman <philippe.dorman@irestful.com>
2014-06-03 13:39:04 -0400

    Made team.php more responsive

--- team.php
+++ team.php
@@ -7,12 +7,12 @@ include './inc/header.php'
      <header>
         <div class="container middle">
             <div class="row">
-                <div class="col-lg-12">                    
+                <div class="col-lg-11">                    
                     <div class="intro-text ">                       
                         <span class="name">Meet the Team</span>
                         <img src="img/white-star.png">
                     </div>
-                    <div class="col-lg-12 intro-text-team"> 
+                    <div class="col-lg-11 intro-text-team"> 
                         <div class="team">
                             <h2>The Nestfinder team</h2>                     
 
