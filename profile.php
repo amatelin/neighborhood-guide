69d408aa996b5d0be9f9a4a9529afbdf7c891022
WidCed <cedric.wident@gmail.com>
2014-06-05 09:34:51 -0400

    Modified mobile navigation

--- profile.php
+++ profile.php
@@ -4,92 +4,72 @@
         <div class="container middle">
             <div class="row">
                 <div class="col-lg-12">                    
-                    <div class="intro-text" id="profile-intro">                       
+                    <div class="intro-text ">                       
                         <span class="name">Profile</span>
                         <img src="img/white-star.png">
                     </div>
-
-                    <div class="intro-text-form" id="profile-wrapper">
-
-                          <div class="profile-pic" >
-                                <img src="img/neighborhoods/neighborhoods/joscar.jpg" class="img-responsive">
-                                <!-- File Button --> 
-                                <div class="form-group">
-                                    <input type="file" id="exampleInputFile">
-                                </div>
-                          </div>
-
-                          <div class="log-form">
-                              <form class="form-horizontal">
-                                  <fieldset>
-                                      <!-- Text input-->
-                                      <div class="form-group">
-                                        <label class="col-md-4 control-label" for="name">Name</label>  
-                                        <div class="col-md-6">
-                                        <input id="name" name="name" placeholder="Type your name" class="form-control input-md" type="text">
-                                          
-                                        </div>
+                    <div class="intro-text-form"> 
+                        <div class="log-form">
+                            <form class="form-horizontal">
+                                <fieldset>
+                                    <!-- Text input-->
+                                    <div class="form-group">
+                                      <label class="col-md-4 control-label" for="name">Name</label>  
+                                      <div class="col-md-6">
+                                      <input id="name" name="name" placeholder="Type your name" class="form-control input-md" type="text">
+                                        
                                       </div>
+                                    </div>
 
-                                      <!-- Text input-->
-                                      <div class="form-group">
-                                        <label class="col-md-4 control-label" for="nickname">Nickname</label>  
-                                        <div class="col-md-6">
-                                        <input id="nickname" name="nickname" placeholder="Type your nickname" class="form-control input-md" type="text">
-                                          
-                                        </div>
+                                    <!-- Text input-->
+                                    <div class="form-group">
+                                      <label class="col-md-4 control-label" for="nickname">Nickname</label>  
+                                      <div class="col-md-6">
+                                      <input id="nickname" name="nickname" placeholder="Type your nickname" class="form-control input-md" type="text">
+                                        
                                       </div>
+                                    </div>
 
-                                      <!-- Text input-->
-                                      <div class="form-group">
-                                        <label class="col-md-4 control-label" for="email">Email</label>  
-                                        <div class="col-md-6">
-                                        <input id="email" name="email" placeholder="Typer your adress mail" class="form-control input-md" type="text">
-                                          
-                                        </div>
+                                    <!-- Text input-->
+                                    <div class="form-group">
+                                      <label class="col-md-4 control-label" for="email">Email</label>  
+                                      <div class="col-md-6">
+                                      <input id="email" name="email" placeholder="Typer your adress mail" class="form-control input-md" type="text">
+                                        
                                       </div>
+                                    </div>
 
-                                      <!-- Password input-->
-                                      <div class="form-group">
-                                        <label class="col-md-4 control-label" for="password">Password</label>
-                                        <div class="col-md-6">
-                                          <input id="password" name="password" placeholder="Typer your password" class="form-control input-md" type="password">
-                                          
-                                        </div>
+                                    <!-- Password input-->
+                                    <div class="form-group">
+                                      <label class="col-md-4 control-label" for="password">Password</label>
+                                      <div class="col-md-6">
+                                        <input id="password" name="password" placeholder="Typer your password" class="form-control input-md" type="password">
+                                        
                                       </div>
+                                    </div>
 
-
-                                      <!-- Datebirth input-->
-                                      <div class="form-group">
-                                        <label class="col-md-4 control-label" for="datebirth">Date of Birth</label>
-                                        <div class="col-md-6">
-                                          <input id="datebirth" name="datebirth" placeholder="DD-MM-YYYY" class="datepicker form-control input-md">
-                                       </div>
+                                    <!-- Multiple Checkboxes (inline) -->
+                                    <div class="form-group">
+                                      <label class="col-md-4 control-label" for="checkboxes"></label>
+                                      <div class="col-md-4">
+                                        <label class="checkbox-inline" for="checkboxes-0">
+                                          <input name="checkboxes" id="checkboxes-0" value="" type="checkbox">
+                                          I've read and i agree with NestFinder's rules.
+                                        </label>
                                       </div>
+                                    </div>
 
-                                      <!-- Text input-->
-                                      <div class="form-group">
-                                        <label class="col-md-4 control-label" for="homeTown">Home Town</label>  
-                                        <div class="col-md-6">
-                                        <input id="homeTown" name="homeTown" placeholder="Typer your home town" class="form-control input-md" type="text">
-                                          
-                                        </div>
+                                    <!-- Button -->
+                                    <div class="form-group">
+                                      <label class="col-md-4 control-label" for="submit"></label>
+                                      <div class="col-md-4">
+                                        <button id="submit" name="submit" class="btn btn-inverse">Register</button>
                                       </div>
+                                    </div>
+                                </fieldset>
+                            </form>
 
-
-
-                                      <!-- Button -->
-                                      <div class="form-group">
-                                        <label class="col-md-4 control-label" for="submit"></label>
-                                        <div class="col-md-4">
-                                          <button id="submit" name="submit" class="btn btn-inverse">Save</button>
-                                        </div>
-                                      </div>
-
-                                  </fieldset>
-                              </form>
-                          </div>   
-
+                        </div>
                     </div>                    
                 </div>
             </div>            
--- quartier.php
+++ quartier.php
@@ -182,12 +182,6 @@
                     </div>
                 </div>
             {{/each}}
-            <ul class="pagination">
-                {{#page this.nb this.page}}
-                    <li {{#ifEq page value}}class="active"{{/ifEq}}><a href="#">{{value}}</a></li>
-                {{/page}}
-            </ul>
-
         </script>
 
     <?php include './inc/footer.php'; ?>
