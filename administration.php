ebe1550a133606aa6562e9ec193f031d0c8eda1d
Vincent Fortin <Nitrofski@gmail.com>
2014-06-10 15:47:12 -0400

    Added basic collapsing left menu

--- administration.php
+++ administration.php
@@ -4,117 +4,70 @@ $page = "team";
 include './inc/header.php'
 ?>
     <div id="admin-wrapper">
-        <div class="section-title">
-            <h1 class="name">Administration</h1>
-            <img src="img/white-star.png">
+        <div class="col-lg-12 text-center">
+            <h2>Administration</h2>
+            <hr class="star-primary">
+        </div>
+        <ul id="admin-menu">
+            <li class="item-group"><a href="#">This</a>
+                <ul>
+                    <li><a href="#">Sub option 1</a></li>
+                    <li><a href="#">Sub option 2</a></li>
+                    <li>Sub option 3</li>
+                    <li>Sub option 4</li>
+                </ul>
+            </li>
+            <li>is</li>
+            <li>just</li>
+            <li>a</li>
+            <li>lot</li>
+            <li>of</li>
+            <li>useless</li>
+            <li class="item-group"><a href="#">lines</a>
+                <ul>
+                    <li><a href="#">Sub option 1</a></li>
+                    <li><a href="#">Sub option 2</a></li>
+                    <li>Sub option 3</li>
+                    <li>Sub option 4</li>
+                </ul>
+            </li>
+            <li>to</li>
+            <li>simulate</li>
+            <li>a</li>
+            <li>menu</li>
+            <li>with</li>
+            <li>a</li>
+            <li>whole</li>
+            <li class="item-group"><a href="#">bunch</a>
+                <ul>
+                    <li><a href="#">Sub option 1</a></li>
+                    <li><a href="#">Sub option 2</a></li>
+                    <li>Sub option 3</li>
+                    <li>Sub option 4</li>
+                </ul>
+            </li>
+            <li>of</li>
+            <li>various</li>
+            <li>options</li>
+            <li>that</li>
+            <li>I</li>
+            <li>still</li>
+            <li>had</li>
+            <li>to</li>
+            <li>make</li>
+            <li>bigger</li>
+            <li>so</li>
+            <li>it</li>
+            <li>took</li>
+            <li>enough</li>
+            <li>space</li>
+            <li>for</li>
+            <li>my</li>
+            <li>liking.</li>
+        </ul>
+        <div id="admin-content">
             <?php include './inc/breadcrumb.php' ?>
         </div>
-        <div class="content">
-            <?php
-                include './inc/menu.php';
-            ?>
-        </div>
     </div>
-    <section class="adjustement">
-        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-              </div>
-              <div class="modal-body">
-                <div class="log-form">
-                    <form class="form-horizontal">
-                        <fieldset>
 
-                            <script src="js/form.js"></script>
-
-                            <!-- Choice Categories -->
-                            <div class="form-group">
-                                <label class="col-md-4 control-label" for="selectbasic">Select Categorie</label>
-                                <div class="col-md-4">
-                                    <select id="selectbasic" name="selectbasic" class="form-control">
-                                        <option value="1" disabled selected>Label</option>
-                                        <optgroup label="Entertainement">
-                                            <option value="2">Cinema</option>
-                                            <option value="3">Theatre</option>
-                                        </optgrounp>
-                                        <optgroup label="Restaurants">
-                                            <option value="4">Cafe</option>
-                                            <!--<option value="5">Resto</option>-->
-                                        </optgrounp>
-                                    </select>
-                                </div>
-                            </div>
-                            <div id="box">
-                            </div>
-
-                            <!-- Default form before -->
-                            <script type="text/template" id="input-before">
-                                <div class="form-group">
-                                  <label class="col-md-4 control-label" for="name">Name</label>
-                                  <div class="col-md-8">
-                                      <input id="name" name="name" placeholder="Enter the name" class="form-control input-md" type="text">
-                                  </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="col-md-4 control-label" for="desc">Description</label>
-                                    <div class="col-md-8">
-                                        <textarea class="form-control" id="desc" name="desc" placeholder="Enter the description"></textarea>
-                                    </div>
-                                </div>
-                            </script>
-
-                            <!-- Default form after -->
-                            <script type="text/template" id="input-after">
-                            <button type="submit" id="submit" name="submit" class="btn btn-primary">Send</button>
-                            </script>
-
-                            <!-- Text input-->
-                            <script type="text/template" id="input-text">
-                                <div class="form-group">
-                                  <label class="col-md-4 control-label" for="{{name}}">{{name}}</label>
-                                  <div class="col-md-8">
-                                      <input id="{{name}}" name="{{name}}" placeholder="{{placeholder}}" class="form-control input-md" type="text">
-                                  </div>
-                                </div>
-                            </script>
-
-                            <!-- Textarea -->
-                            <script type="text/template" id="input-textarea">
-                                <div class="form-group">
-                                    <label class="col-md-4 control-label" for="{{name}}">{{name}}</label>
-                                    <div class="col-md-8">
-                                        <textarea class="form-control" id="{{name}}" name="{{name}}" placeholder="{{placeholder}}"></textarea>
-                                    </div>
-                                </div>
-                            </script>
-
-                            <!-- Select -->
-                            <script type="text/template" id="input-select">
-                                <div class="form-group">
-                                    <label class="col-md-4 control-label" for="{{name}}">{{name}}</label>
-                                    <div class="col-md-4">
-                                        <select id="{{name}}" name="{{name}}" class="form-control">
-                                            {{#each options}}
-                                                <option value="{{this}}">{{this}}</option>
-                                            {{/each}}
-                                        </select>
-                                    </div>
-                                </div>
-                            </script>
-
-                        </fieldset>
-                    </form>
-                </div>
-              </div>
-        <!--    <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>-->
-            </div>
-        </div>
-    </div>
-</section>
- <?php include './inc/footer.php' ?>
+<?php include './inc/footer.php' ?>
